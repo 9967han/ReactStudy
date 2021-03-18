8de40724c006a4b23201cdbf1f648340cedbe15a
sangwook <9967han@naver.com>
2021-03-18 21:02:30 +0900

    배열에 데이터 삽입하기

--- react-tutorials/contact-app/src/App.js
+++ react-tutorials/contact-app/src/App.js
@@ -1,10 +1,32 @@
+import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
-function App() {
-  return (
-    <div >
-        <PhoneForm />
-    </div>
-  );
+class App extends Component {
+
+  id = 0;
+
+  state = {
+    information: [],
+    
+  }
+
+  handleCreate = (data) => {
+    const {information} = this.state;
+    this.setState({
+      information : information.concat(Object.assign({}, data, {
+        id: this.id++
+      }))
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate}/>
+        {JSON.stringify(this.state.information)}
+      </div>
+    );
+  }
 }
+
 export default App;
