295da92db6675291a6a475f75d3e976d5cf3e795
sangwook <9967han@naver.com>
2021-03-19 18:25:32 +0900

    이름으로 전화번호 찾기

--- react-tutorials/contact-app/src/App.js
+++ react-tutorials/contact-app/src/App.js
@@ -4,10 +4,33 @@ import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
 
-  id = 0;
+  id = 3;
 
   state = {
-    information: [], 
+    information: [
+      {
+        id : 0,
+        name : '홍길동',
+        phone : '010-0000-0000'
+      },
+      {
+        id : 1,
+        name : '아이유',
+        phone : '010-0000-0001'
+      },
+      {
+        id : 2,
+        name : '이수현',
+        phone : '010-0000-0002'
+      },
+    ], 
+    keyword : ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword : e.target.value
+    })
   }
 
   handleRemove = (id) => {
@@ -47,7 +70,14 @@ class App extends Component {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate}/>
-        <PhoneInfoList data={this.state.information} onRemove={this.handleRemove} onUpdate={this.handleUpdate}/>
+        <input value={this.state.keyword} onChange={this.handleChange} placeholder="검색..."/>
+        <PhoneInfoList 
+          data={this.state.information.filter(
+            n => n.name.indexOf(this.state.keyword) > -1
+          )}
+          onRemove={this.handleRemove} 
+          onUpdate={this.handleUpdate}
+        />
       </div>
     );
   }
