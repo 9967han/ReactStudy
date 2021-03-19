a85604a3321936ca9b7e55919c7818a0520a457b
sangwook <9967han@naver.com>
2021-03-19 18:32:25 +0900

    Ref를 통하여 DOM에 직접 접근하기

--- react-tutorials/contact-app/src/components/PhoneForm.js
+++ react-tutorials/contact-app/src/components/PhoneForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class PhoneForm extends Component {
+    input = React.createRef();
     state = {
         name : '',
         phone : '',
@@ -17,13 +18,23 @@ class PhoneForm extends Component {
             name : '',
             phone : ''
         });
+        this.input.current.focus();
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input name="name" placeholder = "이름" onChange={this.handleChange} value={this.state.name}/>
-                <input name="phone" placeholder = "전화번호" onChange={this.handleChange} value={this.state.phone}/>
+                <input 
+                    name="name" 
+                    placeholder = "이름" 
+                    onChange={this.handleChange} 
+                    value={this.state.name}
+                    ref={this.input}/>
+                <input 
+                    name="phone" 
+                    placeholder = "전화번호" 
+                    onChange={this.handleChange}
+                    value={this.state.phone}/>
                 <button type="submit">등록</button>
                 <div>
                     {this.state.name}
