3ba6398e588cd8ef8cc18237aa7af9c83a3669e3
sangwook <9967han@naver.com>
2021-08-24 15:47:33 +0900

    Post 요청 처리

--- nodeStudy/app.js
+++ nodeStudy/app.js
@@ -1,11 +1,15 @@
 // nodemon app.js로 서버 실행
 var express = require('express')
+var app = express() // express 함수 실
 
-var app = express() // express 함수 실행
 app.listen(3000, function(){ 
     // 3000포트로 실행하고 대기, 비동기로 인해 나중에 실행됨
     console.log("start! express server port 3000")
 });
+
+app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 //url : / route to main.html
 app.get('/', function(req, res){
@@ -17,9 +21,7 @@ app.get('/main', function(req, res){
     res.sendFile(__dirname + "/public/main.html")
 });
 
-//static file(.js) routing : public 디렉터리 내 파일들은 static으로 요청시 내려받을 수 있음.
-app.use(express.static("public"))
-
-/*app.post('/search', function(req, res){
-    res.sendFile(__dirname + "/public/main.html")
-});*/
+app.post('/email_post', function(req,res){
+    res.send("post respond");
+    console.log(req.body.email);
+});
